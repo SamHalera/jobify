1e6cab597e601a32ccb81e81838deaa072f38a3d
SamHalera <samuel.halera@gmail.com>
2019-11-16 19:14:51 +0100

    template joboffers

---
+++ src/Controller/JobOfferController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class JobOfferController extends AbstractController
+{
+
+    /**
+     * @Route("/jobs", name="app_jobs_list")
+     */
+    public function JobOffersIndex()
+    {
+
+        return $this->render('job_offers/list.html.twig');
+    }
+}
