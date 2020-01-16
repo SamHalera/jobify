267662be8b9bf712e7a1c9db001d07512337755b
Samuel <samuel.halera@gmail.com>
2020-01-16 16:54:02 +0100

    Edit Profile

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\AccountEditFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Doctrine\ORM\EntityManagerInterface;
 
 /**
  * @IsGranted("ROLE_USER")
@@ -17,9 +22,38 @@ class AccountController extends BaseController
      */
     public function index()
     {
-        //dd($this->getUser()->getEmail());
+
+        //dd($this->getUser()->getCompany());
         return $this->render('account/index.html.twig', [
             'controller_name' => 'AccountController',
         ]);
+    }
+
+    /**
+     * @Route("/edit-account/{id}", name="app_edit_account")
+     */
+    public function edit(EntityManagerInterface $em, $id, Request $request)
+    {
+        $repository = $em->getRepository(User::class);
+        $user = $repository->find($id);
+
+        $form = $this->createForm(AccountEditFormType::class, $user);
+        
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $user = $form->getData();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('app_account');
+
+        }
+
+        return $this->render('account/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
+
     }
 }
